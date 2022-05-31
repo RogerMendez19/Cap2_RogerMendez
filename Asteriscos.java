675b071ab70854750e2379b52dc7105d84c88d80
RogerMendez19 <106356644+RogerMendez19@users.noreply.github.com>
2022-05-30 18:57:31 -0600

    Add files via upload

---
+++ Asteriscos.java
@@ -0,0 +1,17 @@
+/*Escriba una aplicación que muestre un cuadro, un óvalo, una flecha
+y un diamante usando asteriscos (*)*/
+public class Asteriscos
+{
+    public static void main(String[]args)
+    {
+        System.out.println("*********      ***        *         *     ");
+        System.out.println("*       *   *       *    ***      *   *   ");
+        System.out.println("*       *  *         *  *****    *     *  ");
+        System.out.println("*       *  *         *    *     *       * ");
+        System.out.println("*       *  *         *    *    *         *");
+        System.out.println("*       *  *         *    *     *       * ");
+        System.out.println("*       *  *         *    *      *     *  ");
+        System.out.println("*       *   *       *     *       *  *    ");
+        System.out.println("*********      ***        *         *     ");
+    }
+}
